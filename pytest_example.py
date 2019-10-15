e0245d78b607bfae6c515cc17c420afed8dc4641
acse-cq419 <56035082+acse-cq419@users.noreply.github.com>
2019-10-15 11:42:42 +0100

    Update pytest_example.py

--- pytest_example.py
+++ pytest_example.py
@@ -5,3 +5,5 @@ def test_solve_quadratic():
     assert(foo.solve_quadratic(1,0,-1)==(-1.0,1.0))
     assert(foo.solve_quadratic(1,0,0)==0.0)
     assert(foo.solve_quadratic(1,0,1) is None)
+    
+# first attampt
